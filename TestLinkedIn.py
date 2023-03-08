55aff9ac46d56ed66c65cd541cb1d0133bf23620
Xzawier1777 <xzawier1777@hotmail.com>
2023-03-08 11:45:28 +0100

    LinkedIn 08.03.2023

--- TestLinkedIn.py
+++ TestLinkedIn.py
@@ -1,2 +1,7 @@
-r = 'C'
-print(int(r, 16))
+# Intéressante façon de traiter cette liste
+lst1 = [1, 2, 3, 4, 5]
+lst2 = [i*2 if i%2==0 else i**2 for i in lst1]
+print(lst2)
+
+# r = 'C'
+# print(int(r, 16))
