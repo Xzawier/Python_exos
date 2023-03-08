e0e10ee972c5ed5f453e518706326630f55897bc
Xzawier1777 <xzawier1777@hotmail.com>
2023-03-08 09:59:52 +0100

    08.03.2023

--- TestLinkedIn.py
+++ TestLinkedIn.py
@@ -1,2 +1,12 @@
-r = 'C'
-print(int(r, 16))
+# r = 'C'
+# print(int(r, 16))
+
+# lst = [1,2,3,4,5]
+# print(lst[::])
+# print(lst[::-1])
+# print(lst[::-2])
+# print(lst[::-2][::-1])
+# https://www.linkedin.com/feed/update/urn:li:activity:7036198569481027584?utm_source=share&utm_medium=member_desktop
+
+for i in range(3):
+    print(i,end=' ')
