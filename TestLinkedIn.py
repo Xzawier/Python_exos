ffc47a5b3ff995473503ca587925e85fcd93e6b0
Xzawier1777 <xzawier1777@hotmail.com>
2023-03-08 23:47:34 +0100

    08 mars last du jour

--- TestLinkedIn.py
+++ TestLinkedIn.py
@@ -1,7 +1,18 @@
-# Intéressante façon de traiter cette liste
-lst1 = [1, 2, 3, 4, 5]
-lst2 = [i*2 if i%2==0 else i**2 for i in lst1]
-print(lst2)
+def func(x):
+
+    if(x==0 or x==1):
+
+        return 1
+
+    return func(x-1)*x
+
+func(1)
+# print("Hellow")
+
+# # Intéressante façon de traiter cette liste
+# lst1 = [1, 2, 3, 4, 5]
+# lst2 = [i*2 if i%2==0 else i**2 for i in lst1]
+# print(lst2)
 
 # r = 'C'
 # print(int(r, 16))
