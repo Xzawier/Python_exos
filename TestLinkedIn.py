156064a6d6e4201f2d60aeb40af8228cd6977682
Xzawier1777 <xzawier1777@hotmail.com>
2023-03-08 11:37:32 +0100

    Test 08.03.23

--- TestLinkedIn.py
+++ TestLinkedIn.py
@@ -1,3 +1,11 @@
+
+
+
+# # 08.03.2023
+
+# arr = [1,2,2,3,4,5,5,5,6,7,8,8,9]
+# print(tuple(set([x for x in arr if arr.count(x)>1])))
+
 # r = 'C'
 # print(int(r, 16))
 
@@ -8,5 +16,7 @@
 # print(lst[::-2][::-1])
 # https://www.linkedin.com/feed/update/urn:li:activity:7036198569481027584?utm_source=share&utm_medium=member_desktop
 
-for i in range(3):
-    print(i,end=' ')
+# for i in range(3):
+#     print(i,end=' ')
+
+
