2036a79edbe4818805b0fc8952821afbbc970148
Xzawier <xzawier1777@hotmail.com>
2023-03-10 14:39:22 +0100

    10.03.20223

--- TestLinkedIn.py
+++ TestLinkedIn.py
@@ -1,12 +1,17 @@
-def func(x):
 
-    if(x==0 or x==1):
+# 10.03.20223
+for i in range(1,8,3):
+    print(i,end='')
 
-        return 1
+# def func(x):
 
-    return func(x-1)*x
+#     if(x==0 or x==1):
 
-func(6) #ça maaarche paaaas
+#         return 1
+
+#     return func(x-1)*x
+
+# func(6) #ça maaarche paaaas
 # print("Hellow")
 
 # # Intéressante façon de traiter cette liste
@@ -16,18 +21,18 @@ func(6) #ça maaarche paaaas
 
 # r = 'C'
 # print(int(r, 16))
-g = {3, 4, 5}
-h = {5, 2, 3}
-print(h - g)
+# g = {3, 4, 5}
+# h = {5, 2, 3}
+# print(h - g)
 
-i = {3, 4}
-j = {5, 2}
-print(j - i)
+# i = {3, 4}
+# j = {5, 2}
+# print(j - i)
 
-m = {4, 5, 4}
-n = {2, 3, 5}
-print(n - m)
+# m = {4, 5, 4}
+# n = {2, 3, 5}
+# print(n - m)
 
-p = {3, 4, 5, 4}
-q = {5, 2, 3, 5}
-print(q - p)
+# p = {3, 4, 5, 4}
+# q = {5, 2, 3, 5}
+# print(q - p)
