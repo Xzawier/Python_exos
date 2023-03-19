3117892d8a13b0fbe2e373805fb64c2fb865b824
Xzawier1777 <xzawier1777@hotmail.com>
2023-03-19 01:19:41 +0100

    class V

---
+++ Test_LinkedIn2.py
@@ -0,0 +1,13 @@
+class V:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return V(self.x + other.x, self.y + other.y)
+    
+v1 = V(1, 2)
+v2 = V(3, 4)
+v3 = v1 + v2
+
+print(v3.x + v3.y)
