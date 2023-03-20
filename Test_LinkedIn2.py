3b7ddfb9fd5a5db938bd90a67c68bbb2ba8b2e80
Xzawier1777 <xzawier1777@hotmail.com>
2023-03-20 20:39:57 +0100

    list

--- Test_LinkedIn2.py
+++ Test_LinkedIn2.py
@@ -20,3 +20,12 @@ while True:
         break
     print(i)
     i += 1
+
+# ================
+
+list1= {22, 35, 51}
+list1.add((5,1))
+# print(list1) pour voir/tricher
+list2 = {4,51,5,1}
+list2.intersection_update(list1)
+print(list2)
